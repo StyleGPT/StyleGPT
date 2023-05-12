9f9f9bbd3488950bbecd72d1bb645fed2259193d
Kyle Slugg <kyle.slugg@gmail.com>
2023-05-12 17:03:12 -0400

    Updated boilerplate

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div id="main-app-div">
+      <h1>Test Header</h1>
+      <div>User entry and search goes here</div>
+      <div>Results and rendering pane can go here</div>
+    </div>
+  );
+};
+
+export default App;
