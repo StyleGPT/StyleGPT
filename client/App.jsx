cfc7ee8e151acf2ea220d68d977db7f1c3a5fdeb
Eduardo Zayas <ezayasavila@gmail.com>
2023-05-13 12:17:31 -0400

    Added basic query entry form.
    Co-authored-by: Pearl

--- client/App.jsx
+++ client/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './styles.scss';
 import ResultsDisplayPane from './components/ResultsDisplayPane';
 import './hljs-tokyo-night-dark-custom.css';
+import QueryEntryForm from './components/QueryEntry';
 
 const App = () => {
   return (
     <div id="main-app-div">
       <h1>Test Header</h1>
-      <div>User entry and search goes here</div>
+      <div>
+        <QueryEntryForm />
+        </div>
       <div>
         Results and rendering pane can go here
         <ResultsDisplayPane
--- client/components/QueryEntry.jsx
+++ client/components/QueryEntry.jsx
@@ -1,1 +1,16 @@
 import React, { useState, useEffect } from 'react';
+
+const QueryEntryForm = () => {
+    return (
+        <div>
+          <h2>What kind of style would you like?</h2>
+          <form>
+            <input type="text" id="name" name="name" /><br />
+            <button type="submit">Submit</button>
+            <button type="reset">Reset</button>
+          </form>
+        </div>
+    );
+};
+
+export default QueryEntryForm;
