5a03f5b6cabc06002382f2f23fd62e721cc2c317
Kyle Slugg <kyle.slugg@gmail.com>
2023-05-13 11:22:36 -0400

    Added styling for CSS code loading

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './styles.css';
+import './styles.scss';
 import ResultsDisplayPane from './components/ResultsDisplayPane';
-//import 'highlight.js/styles/github.css';
+import './hljs-tokyo-night-dark-custom.css';
 
 const App = () => {
   return (
