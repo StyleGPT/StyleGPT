da238fa9a01f28b27719d500e3590cf647813656
Eduardo Zayas <ezayasavila@gmail.com>
2023-05-14 17:38:15 -0400

    Created basic query handler for login.

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 const LoginForm = () => {
 
     const [ email, setEmail ] = useState('');
-    const [ passw, setPassw ] = useState('');
+    const [ password, setPassword ] = useState('');
     const [ dataInput, setDataInput ] = useState('');
     const submitThis = () => {
-        const info = {email: email, passw: passw}
+        const info = {email: email, password: password}
         setDataInput(info);
     }
 
@@ -18,8 +18,8 @@ const LoginForm = () => {
                     <input type='text' name='email' id='email' value={email} onChange={e=>setEmail(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor='passw'>Password</label>
-                    <input type='text' name='passw' id='passw' value={passw} onChange={(e=>setPassw(e.target.value))}/>
+                    <label htmlFor='password'>Password</label>
+                    <input type='text' name='password' id='password' value={password} onChange={(e=>setPassword(e.target.value))}/>
                 </div>
                 <button type='submit'>Login</button>
             </form>
@@ -28,3 +28,25 @@ const LoginForm = () => {
 }
 
 export default LoginForm;
+
+
+const handleQuery = (userInfo) => {
+  setReqStatus('waiting');
+  return fetch(`/login`, {
+    method: 'POST',
+    body: userInfo,
+    headers: { 'Content-type': 'text/plain' }
+  })
+    .then((response) => response.text())
+    .then((text) => {
+      if (text === 'logged in') {
+
+      }
+      if (text === 'login failed') {
+        window.alert('Email or password incorrect')
+      }
+    })
+    .catch((err) => {
+      console.log(`Received error while processing login request: ${err}`);
+    });
+};
