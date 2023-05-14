db1fe4103f426f869ad3e5ceb87dd9fad6722cd9
Eduardo Zayas <ezayasavila@gmail.com>
2023-05-14 17:07:24 -0400

    set up state management for login form

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 
 const LoginForm = () => {
+
+    const [ email, setEmail ] = useState('');
+    const [ passw, setPassw ] = useState('');
+    const [ dataInput, setDataInput ] = useState('');
+    const submitThis = () => {
+        const info = {email: email, passw: passw}
+        setDataInput(info);
+    }
+
     return (
         <div>
-            <form action=''>
+            <form action='' onSubmit={submitThis}>
                 <div>
                     <label htmlFor='email'>Email</label>
-                    <input type='text' name='email' id='email'/>
+                    <input type='text' name='email' id='email' value={email} onChange={e=>setEmail(e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor='passw'>Password</label>
-                    <input type='text' name='passw' id='passw'/>
+                    <input type='text' name='passw' id='passw' value={passw} onChange={(e=>setPassw(e.target.value))}/>
                 </div>
                 <button type='submit'>Login</button>
             </form>
