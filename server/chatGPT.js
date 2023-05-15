f77740cae254cd583a79ec1ac452ee0e0c6a6b56
Kyle Slugg <kyle.slugg@gmail.com>
2023-05-15 12:06:29 -0400

    Enabling user API key input

--- server/chatGPT.js
+++ server/chatGPT.js
@@ -1,5 +1,5 @@
-const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
 
 // html is a string that's formatted html.
 const html = `
@@ -23,27 +23,27 @@ const htmlString = html.replace(/\n/g, '').replace(/\s+/g, ' ').trim();
 // make a larger function that includes runcompletion and configuration
 
 // temporary variable to hold open AI key while we figure how to get the the prompt in runcompletion
-const key = process.env.OPENAI_API_KEY;
+//const key = process.env.OPENAI_API_KEY;
 
 // runCompletion calls the API with the necessary inputs
-    // descriptor is the input from the user, taken from req.body.prompt
+// descriptor is the input from the user, taken from req.body.prompt
 async function runCompletion(descriptor, key) {
-    // configuration is where private key is set up
-    const configuration = new Configuration({
-        // apiKey: process.env.OPENAI_API_KEY,
-        apiKey: key
-      });
-    // OpenAIApi constructor takes in the configuration object.
-    const openai = new OpenAIApi(configuration);
-        const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `generate CSS rules for each element of this html ${descriptor} ${htmlString}`,
-            max_tokens:4000,
-            // temperature set to zero to keep responses consistent
-            temperature:0
-        });
-    console.log('response from chatGPT', completion.data.choices[0].text);
-    return completion.data.choices[0].text;
+  // configuration is where private key is set up
+  const configuration = new Configuration({
+    // apiKey: process.env.OPENAI_API_KEY,
+    apiKey: key
+  });
+  // OpenAIApi constructor takes in the configuration object.
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `generate CSS rules for each element of this html to look ${descriptor} ${htmlString}`,
+    max_tokens: 4000,
+    // temperature set to zero to keep responses consistent
+    temperature: 0.5
+  });
+  console.log('response from chatGPT', completion.data.choices[0].text);
+  return completion.data.choices[0].text;
 }
 
 // returns the response from the API
@@ -51,10 +51,9 @@ async function runCompletion(descriptor, key) {
 // console.log(cssResponse);
 
 // make response - main export, holds the configuration, takes in everything passed in from the controller, prompt and the API key
-    // define the prompt
-    // define configuration and API piece
-    // use whatever key is passed in.
+// define the prompt
+// define configuration and API piece
+// use whatever key is passed in.
 // html string could stay outside of it for now
-
 
 module.exports = runCompletion;
